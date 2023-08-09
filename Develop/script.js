2c68cdb379319602d48dc45985091eea8168d0e9
pcstoyle <pcstoyle@hotmail.com>
2023-08-08 18:22:07 -0600

    working on saving user text

--- Develop/script.js
+++ Develop/script.js
@@ -2,31 +2,18 @@ var today = dayjs();
 var currentDayEl = document.getElementById("currentDay");
 
 
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that the code isn't run until the browser has finished rendering all the elements in the html.
-
 $(function () {
-  // TODO: Add code to display the current date in the header of the page.
-  //continual count of current time:
 
   function keepTime() {
     var dateTime = dayjs().format('ddd, MMM D, YYYY h:mm:ss');
     currentDayEl.textContent = dateTime;
   };
-  // setInterval(keepTime, 1000);
   var time = setInterval(() => {
-
     keepTime()
   }, 1000);
 
   keepTime()
 
-
-  //TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? How can Day.js be used to get the current hour in 24-hour time?
-
-  // var timeBlock = document.querySelectorAll(".time-block"); //isolating the time block elements by ID to know which hour the program should be looking at for past/present/future 
-  // var currentHour = today.format('H') //looking at current time in 24 hour format
-
-  //find out which time block we should currently be working in/with - use the div id hour-# to decide 
   function hourColor() {
     var timeBlock = document.querySelectorAll(".time-block");
     var currentHour = parseInt(today.format('H'));
@@ -46,24 +33,70 @@ $(function () {
     })
 
   }
+
   hourColor();
-
-
-
 
   // TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage. HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be useful when saving the description in local storage?
 
-  var saveBtnEl = document.querySelectorAll(".saveBtn");
-  //var userText =
 
-  function saveText(event) {
-    event.preventDefault();
+
+  var saveButton = document.querySelector(".saveBtn");
+  var userInput = document.querySelector(".description");
+  var hourNine = document.getElementById("hour-9");
+  var hourTen = document.getElementById("hour-10").textarea;
+  var hourEleven = document.getElementById("hour-11").textarea;
+  var hourTwelve = document.getElementById("hour-12").textarea;
+  var hourThirteen = document.getElementById("hour-13").textarea;
+  var hourFourteen = document.getElementById("hour-14").textarea;
+  var hourFifteen = document.getElementById("hour-15").textarea;
+  var hourSixteen = document.getElementById("hour-16").textarea;
+  var hourSeventeen = document.getElementById("hour-17").textarea;
+  // var timeBlock = document.querySelectorAll(".time-block")
+  // var toDoNow = hour.id
+
+  // saveLocalInput();
+  
+
+  function saveLocalInput() {
+
+    //local storge key name for each hour
+    hourNine.textContent = localStorage.getItem("hourNine");
+    hourTen = localStorage.getItem("hourTen");
+    hourEleven.textContent = localStorage.getItem("hourEleven");
+    hourTwelve.textContent = localStorage.getItem("hourTwelve");
+    hourThirteen.textContent = localStorage.getItem("hourThirteen");
+    hourFourteen.textContent = localStorage.getItem("hourFourteen");
+    hourFifteen.textContent = localStorage.getItem("hourFifteen");
+    hourSixteen.textContent = localStorage.getItem("hourSixteen");
+    hourSeventeen.textContent = localStorage.getItem("hourSeventeen");
 
   };
 
-  //addEventListener.
-  // TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements. HINT: How can the id attribute of each time-block be used to do this?
+  saveButton.addEventListener("click", function (event) {
+    event.preventDefault();
+console.log("test")
+    var nine = document.getElementById("hour-9").value;
+    var ten = document.getElementById("hour-10").textarea;
+    var eleven = document.getElementById("hour-11").textarea;
+    var twelve = document.getElementById("hour-12").textarea;
+    var thirteen = document.getElementById("hour-13").textarea;
+    var fourteen = document.getElementById("hour-14").textarea;
+    var fifteen = document.getElementById("hour-15").textarea;
+    var sixteen = document.getElementById("hour-16").textarea;
+    var seventeen = document.getElementById("hour-17").textarea;
 
 
+    localStorage.setItem("hourNine", nine);
+    localStorage.setItem("hourTen", ten);
+    localStorage.setItem("hourEleven", eleven);
+    localStorage.setItem("hourTwelve", twelve);
+    localStorage.setItem("hourThirteen", thirteen);
+    localStorage.setItem("hourFourteen", fourteen);
+    localStorage.setItem("hourFifteen", fifteen);
+    localStorage.setItem("hourSixteen", sixteen);
+    localStorage.setItem("hourSeventeen", seventeen);
+    saveLocalInput();
+  })
 
-})
+});
+
